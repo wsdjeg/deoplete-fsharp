2a94ed9084d18cfb3e64c8fb7c81417f5f60ea91
wsdjeg <wsdjeg@outlook.com>
2018-08-25 19:27:55 +0800

    Do not download syntax file

--- install.bash
+++ install.bash
@@ -3,7 +3,3 @@
 git clone --depth 1 'https://github.com/callmekohei/deoplete-fsharp-bin.git'
 mv deoplete-fsharp-bin/bin ./
 rm -rf deoplete-fsharp-bin
-
-mkdir syntax
-wget 'https://raw.githubusercontent.com/fsharp/vim-fsharp/master/syntax/fsharp.vim' -P './syntax/'
-
